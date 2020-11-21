8226bf25d855781903a8e394b400d99a06c81c23
MichaelChen <helloworldsmart3@gmail.com>
2020-11-21 18:32:06 +0800

    Next : hackingwithswift Day1 Doubles and booleans

---
+++ HackingWithSwift/100DaysOfSwift/Day1/Day1Playground.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+//- Variables
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+var favoriteShow = "Orange is the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor who"
+
+//- Strings and integers
+var age = 18
+var population = 9_000_000
+
+var meaningOfLife = 78
+//meaningOfLife = "Forty two" // Error
+
+//- Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over muliple \
+lines
+"""
+
+var quote = "Change the world by being yourself"
+
+var burns = """
+The best laid schemes
+0' mice and men
+Gang aft agley
+"""
+//- Doubles and booleans
+
