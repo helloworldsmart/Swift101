d48d2c841ba6e5aea8c1f9a43dd60dd4cae35a01
MichaelChen <helloworldsmart3@gmail.com>
2021-02-08 22:14:40 +0800

    FMDBTut add openDatabase

--- Appcoda/FMDBTut/FMDBTut/DBManager.swift
+++ Appcoda/FMDBTut/FMDBTut/DBManager.swift
@@ -29,8 +29,40 @@ class DBManager: NSObject {
     
     if !FileManager.default.fileExists(atPath: pathToDatabase) {
       database = FMDatabase(path: pathToDatabase!)
+      
+      if database != nil {
+        if database.open() {
+          do {
+            let createMoviesTableQuery = "create table movies (movieID integer primary key autoincrement not null, title text not null, category text not null, year integer not null, movieURL text, coverURL text not null, watched bool not null default 0, likes integer not null)"
+  //          let createMoviesTableQuery = "create table movies (\(field_MovieID) integer primary key autoincrement not null, \(field_MovieTitle) text not null, \(field_MovieCategory) text not null, \(field_MovieYear) integer not null, \(field_MovieURL) text, \(field_MovieCoverURL) text not null, \(field_MovieWatched) bool not null default 0, \(field_MovieLikes) integer not null)"
+            try database.executeUpdate(createMoviesTableQuery, values: nil)
+            created = true
+          } catch {
+            print("Could not create table.")
+            print(error.localizedDescription)
+          }
+          database.close()
+        } else {
+          print("Could not open the database.")
+        }
+      }
     }
     return created
+  }
+  
+  func openDatabase() -> Bool {
+    if database == nil {
+      if FileManager.default.fileExists(atPath: pathToDatabase) {
+        database = FMDatabase(path: pathToDatabase)
+      }
+    }
+    
+    if database != nil {
+      if database.open() {
+        return true
+      }
+    }
+    return false
   }
   
 }
