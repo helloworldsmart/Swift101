d2ce7b020b1f3546738ca47387c892a29cad0332
MichaelChen <helloworldsmart3@gmail.com>
2021-02-08 23:02:35 +0800

    FMDBTut Feat: 插入記錄

--- Appcoda/FMDBTut/FMDBTut/DBManager.swift
+++ Appcoda/FMDBTut/FMDBTut/DBManager.swift
@@ -75,5 +75,39 @@ class DBManager: NSObject {
     return false
   }
   
+  func insertMovieData() {
+    // Open the database
+    if openDatabase() {
+      if let pathToMoviesFile = Bundle.main.path(forResource: "movies", ofType: "tsv") {
+        do {
+          let moviesFileContents = try String(contentsOfFile: pathToMoviesFile)
+          let moviesData = moviesFileContents.components(separatedBy: "\r\n")
+          
+          var query = ""
+          for movie in moviesData {
+            let movieParts = movie.components(separatedBy: "\t")
+            
+            if movieParts.count == 5 {
+              let movieTitle = movieParts[0]
+              let movieCategory = movieParts[1]
+              let movieYear = movieParts[2]
+              let movieURL = movieParts[3]
+              let movieCoverURL = movieParts[4]
+              
+              query += "insert into movies (\(field_MovieID), \(field_MovieTitle), \(field_MovieCategory), \(field_MovieYear), \(field_MovieURL), \(field_MovieCoverURL), \(field_MovieWatched), \(field_MovieLikes)) values (null, '\(movieTitle)', '\(movieCategory)', \(movieYear), '\(movieURL)', '\(movieCoverURL)', 0, 0);"
+            }
+          }
+          if !database.executeStatements(query) {
+            print("Failed to insert initial data into the database.")
+            print(database.lastError(), database.lastErrorMessage())
+          }
+        } catch {
+          print(error.localizedDescription)
+        }
+      }
+      database.close()
+    }
+  }
+  
 }
 
