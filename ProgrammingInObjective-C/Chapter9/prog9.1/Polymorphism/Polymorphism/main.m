90edf192ef60d021d235738d18337798e05f867b
MichaelChen <helloworldsmart3@gmail.com>
2021-02-22 23:27:26 +0800

    Programming in Objective-C: Chapter9 9.2

--- ProgrammingInObjective-C/Chapter9/prog9.1/Polymorphism/Polymorphism/main.m
+++ ProgrammingInObjective-C/Chapter9/prog9.1/Polymorphism/Polymorphism/main.m
@@ -11,29 +11,22 @@
 
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
+    id  dataValue;
     Fraction *f1 = [[Fraction alloc] init];
-    Fraction *f2 = [[Fraction alloc] init];
-    Fraction *fracResult;
     Complex *c1 = [[Complex alloc] init];
-    Complex *c2 = [[Complex alloc] init];
-    Complex *compResult;
     
-    [f1 setTo:1 over:10];
-    [f2 setTo:2 over:15];
+    [f1 setTo: 2 over: 5];
+    [c1 setReal: 10.0 andImaginary: 2.5];
     
-    [c1 setReal: 18.0 andImaginary: 2.5];
-    [c2 setReal: -5.0 andImaginary: 3.2];
+    // first dataValue get a fraction
     
-    [c1 print]; NSLog(@"    +"); [c2 print];
-    NSLog(@"---------");
-    compResult = [c1 add: c2];
-    [compResult print];
-    NSLog(@"\n");
+    dataValue = f1;
+    [dataValue print];
     
-    [f1 print]; NSLog(@"    +"); [f2 print];
-    NSLog(@"----");
-    fracResult = [f1 add: f2];
-    [fracResult print];
+    // now dataValue gets a complex number
+    
+    dataValue = c1;
+    [dataValue print];
   }
   return 0;
 }
