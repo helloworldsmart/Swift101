2d7aabc1f266036abac477527c2be25a56674f55
MichaelChen <helloworldsmart3@gmail.com>
2021-03-03 21:26:13 +0800

    Algorithms

--- swift-algorithm-club/Algorithms/MyPlayground.playground/Contents.swift
+++ swift-algorithm-club/Algorithms/MyPlayground.playground/Contents.swift
@@ -43,7 +43,105 @@ func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
   b = tmp;
 }
 
+func buying1(money: Int, price: Int, capacity: Int, volume: Int) {
+  if money >= price {
+    if capacity >= volume {
+      print("Start buying...")
+      print("\(money-price) money left after buying.")
+      print("\(capacity-volume) capacity left after buying")
+    } else {
+      print("No enough capacity")
+    }
+  } else {
+    print("No enough money")
+  }
+}
 
+func buying2(money: Int, price: Int, capacity: Int, volume: Int) {
+  guard money > price else {
+    print("No enough money")
+    return
+  }
+  guard capacity >= volume else {
+    print("No enough capacity")
+    return
+  }
+  
+  print("Start buying....")
+  print("\(money-price) money after buying.")
+  print("\(capacity-volume) capacity left after buying.")
+}
 
+func log(message: String) {
+  print("log: \(message)!")
+}
 
+log(message: "memory warning")
 
+func logString(string: String) -> String {
+  return "log: " + string
+}
+
+let logStr = logString(string: "memory warning!")
+print("\(logStr)")
+
+func logMessage(_ message: String) {
+  print("log: \(message)!")
+}
+
+logMessage("memory warning")
+
+func addInt(_ a: Int, _ b: Int) {
+  print("Sum is \(a + b)")
+}
+
+addInt(3, 4)
+
+var originalArr = [2, 1, 3]
+
+func removeLastInArray(_ array: inout [Int]) {
+  array.removeLast()
+}
+
+print("\n ===== before removing: \(originalArr)")
+
+removeLastInArray(&originalArr)
+print("\n ===== after removing: \(originalArr)")
+
+// immutable array
+let immutableNumbers: [Int] = [1, 3, 5, 4, 4, 1]
+
+// mutable array
+var mutableNumbers: [Int] = [2, 1, 5, 4, 1, 3]
+
+// iteration 1
+for value in mutableNumbers {
+//  if let index = mutableNumbers.index(of: value) {
+  if let index = mutableNumbers.firstIndex(of: value) {
+    print("Index of \(value) is \(index)")
+  }
+}
+
+// iteration 2
+mutableNumbers.forEach { (value) in
+  if let index = mutableNumbers.firstIndex(of: value) {
+    print("Index of \(value) is \(index)")
+  }
+}
+
+// iteration 3
+for (index, value) in mutableNumbers.enumerated() {
+  print("Item \(index + 1): \(value)")
+}
+
+mutableNumbers.append(11)
+
+mutableNumbers.insert(42, at: 4)
+
+mutableNumbers.swapAt(0, 1)
+
+mutableNumbers.remove(at: 0)
+
+mutableNumbers.removeFirst()
+
+mutableNumbers.removeAll()
