ce3d51727fb4069a780af9e8fc579758af4a6d2b
MichaelChen <helloworldsmart3@gmail.com>
2021-03-29 17:58:25 +0800

    CS193P Lecture 2

--- CS193P/Memorize/Memorize/EmojiMemoryGame.swift
+++ CS193P/Memorize/Memorize/EmojiMemoryGame.swift
@@ -8,7 +8,15 @@
 import SwiftUI
 
 class EmojiMemoryGame {
-  private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 2) { _ in "😀" }
+  private var model: MemoryGame<String> = createMemoryGame()
+  
+  static func createMemoryGame() -> MemoryGame<String> {
+    let emojis: Array<String> = ["👻", "🎃"]
+    return MemoryGame<String>(numberOfPairsOfCards: 2) { pairIndex in
+      return emojis[pairIndex]
+      
+    }
+  }
   
   // MARK: Access to the Model
   
