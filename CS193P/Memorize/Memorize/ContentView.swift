86c416586f0e648f1d4d136ba69ff3a84935b59d
MichaelChen <helloworldsmart3@gmail.com>
2021-03-29 12:53:27 +0800

    CS193P Lecture 1

--- CS193P/Memorize/Memorize/ContentView.swift
+++ CS193P/Memorize/Memorize/ContentView.swift
@@ -9,22 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      HStack(content: {
-        ForEach(0..<4, content: { index in
-           ZStack(content: {
+      HStack {
+        ForEach(0..<4) { index in
+           ZStack {
             RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
             RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
             Text("👻")
-          })
-        })
-      })
-      .padding()
-      .foregroundColor(Color.orange)
-      .font(Font.largeTitle)
+          }
+        }
+      }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
     }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
