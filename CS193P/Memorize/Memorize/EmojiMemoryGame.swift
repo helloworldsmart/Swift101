81fa66c812553282e3de2305cc911bda23013dde
MichaelChen <helloworldsmart3@gmail.com>
2021-03-30 18:14:49 +0800

    CS193P Lecture 2

--- CS193P/Memorize/Memorize/EmojiMemoryGame.swift
+++ CS193P/Memorize/Memorize/EmojiMemoryGame.swift
@@ -11,8 +11,8 @@ class EmojiMemoryGame {
   private var model: MemoryGame<String> = createMemoryGame()
   
   static func createMemoryGame() -> MemoryGame<String> {
-    let emojis: Array<String> = ["👻", "🎃"]
-    return MemoryGame<String>(numberOfPairsOfCards: 2) { pairIndex in
+    let emojis: Array<String> = ["👻", "🎃", "🕷"]
+    return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
       return emojis[pairIndex]
       
     }
--- CS193P/Memorize/Memorize/MemoryGame.swift
+++ CS193P/Memorize/Memorize/MemoryGame.swift
@@ -10,7 +10,7 @@ import Foundation
 struct MemoryGame<CardContent> {
   var cards: Array<Card>
   
-  func choose(card: Card)  {
+  func choose(card: Card) {
     print("card chosen: \(card)")
   }
   
@@ -18,14 +18,15 @@ struct MemoryGame<CardContent> {
     cards = Array<Card>()
     for pairIndex in 0..<numberOfPairsOfCards {
       let content = cardContentFactory(pairIndex)
-      cards.append(Card(isFaceUp: false, isMatched: false, content: content))
-      cards.append(Card(isFaceUp: false, isMatched: false, content: content))
+      cards.append(Card(isFaceUp: false, isMatched: false, content: content, id: pairIndex * 2))
+      cards.append(Card(isFaceUp: false, isMatched: false, content: content, id: pairIndex * 2 + 1))
     }
   }
   
-  struct Card {
-    var isFaceUp: Bool
-    var isMatched: Bool
+  struct Card: Identifiable {
+    var isFaceUp: Bool = true
+    var isMatched: Bool = false
     var content: CardContent
+    var id: Int
   }
 }
