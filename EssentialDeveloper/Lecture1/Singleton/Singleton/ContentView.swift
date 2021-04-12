992a162aefbf4e3062b742040a2f9b7e49a7f2f0
MichaelChen <helloworldsmart3@gmail.com>
2021-04-12 23:57:03 +0800

    essential developer

---
+++ EssentialDeveloper/Lecture1/Singleton/Singleton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Singleton
+//
+//  Created by Michael on 2021/4/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EssentialDeveloper/Lecture1/Singleton/Singleton/Singleton.swift
@@ -0,0 +1,58 @@
+//
+//  Singleton.swift
+//  Singleton
+//
+//  Created by Michael on 2021/4/12.
+//
+
+import Foundation
+import UIKit
+
+// Main Module
+
+extension ApiClient {
+  func login(completion: (LoggedInUser) -> Void) {}
+}
+
+extension ApiClient {
+  func loadFeed(completion: ([FeedItem]) -> Void) {}
+}
+
+
+// Api Module
+
+class ApiClient {
+  static let shared = ApiClient()
+  
+  func execute(_ : URLRequest, completion: (Data) -> Void) {}
+}
+
+
+// Login Module
+struct LoggedInUser {}
+
+class LoginViewController: UIViewController {
+  var login: (((LoggedInUser) -> Void) -> Void)?
+  
+  func didTapLoginButton() {
+    login? { user in
+      // show feed screen
+    }
+  }
+}
+
+// Feed Module
+
+struct FeedIten {}
+
+class  FeedViewController: UIViewController {
+  var loadFeed: ((([FeedIten]) -> Void) -> Void)?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    loadFeed? { loadedItems in
+      //update UI
+    }
+  }
+}
