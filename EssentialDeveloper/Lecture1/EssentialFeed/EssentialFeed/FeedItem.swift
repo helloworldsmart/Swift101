fe7963fc346ec7be520a092bc388f4a9d9c70423
MichaelChen <helloworldsmart3@gmail.com>
2021-04-18 16:39:34 +0800

    Create EssentialFeed framework

---
+++ EssentialDeveloper/Lecture1/EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Michael on 2021/4/18.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
---
+++ EssentialDeveloper/Lecture1/EssentialFeed/EssentialFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialFeed
+//
+//  Created by Michael on 2021/4/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
