1f86680dbbe5fb6125f98f412afc2eccfcccdbc5
MichaelChen <helloworldsmart3@gmail.com>
2021-04-18 20:14:08 +0800

    Refactor test to remove duplication

--- EssentialDeveloper/Lecture2/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialDeveloper/Lecture2/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -25,24 +25,10 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    func get(from url: URL) {
-        requestedURL = url
-    }
-    
-    var requestedURL: URL?
-}
-
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-//        let client = HTTPClient()
-//        let client = HTTPClient.shared
-        let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()
-//        HTTPClient.shared = client
-        _ = RemoteFeedLoader(url: url, client: client)
-        
+        let (_, client) = makeSUT()
         XCTAssertNil(client.requestedURL)
     }
     
@@ -52,13 +38,29 @@ class RemoteFeedLoaderTests: XCTestCase {
 //        let client = HTTPClient()
 //        let client = HTTPClient.shared
         let url = URL(string: "https://a-given-url.com")
-        let client = HTTPClientSpy()
+        
 //        HTTPClient.shared = client
-        let sut = RemoteFeedLoader(url: url!, client: client)
+        let (sut, client) = makeSUT(url: url!)
         sut.load()
 
 //        XCTAssertNil(client.requestedURL)
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) ->(sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut =  RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
     
     override func setUpWithError() throws {
