f962de965a22ccc378f6ce6e2b47085ef3162646
MichaelChen <helloworldsmart3@gmail.com>
2021-04-19 23:33:53 +0800

    MVVMTutorial2

---
+++ Appcoda/MVVMTutorial2/MVVMTutorial2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMTutorial2
+//
+//  Created by Michael on 2021/4/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
