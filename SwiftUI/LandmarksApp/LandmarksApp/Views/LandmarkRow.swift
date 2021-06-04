02fe09112ab158e2d1b8c50112916b0904f7aac1
MichaelChen <helloworldsmart3@gmail.com>
2021-06-04 20:53:03 +0800

    [SwiftUI]: building lists and navigation part1

---
+++ SwiftUI/LandmarksApp/LandmarksApp/Views/LandmarkRow.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarkRow.swift
+//  LandmarksApp
+//
+//  Created by Michael on 2021/5/29.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+        
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
+
+
