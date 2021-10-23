a07c386dfd3a4fad45b9338c6e38e797369eabc0
MichaelChen <helloworldsmart3@gmail.com>
2021-10-23 22:47:13 +0800

    [Raywenderlich]: SOLID Principles for iOS Apps

--- raywenderlich/SOLID/ExpenseTracker/Storage/Persistence.swift
+++ raywenderlich/SOLID/ExpenseTracker/Storage/Persistence.swift
@@ -49,4 +49,37 @@ struct PersistenceController {
       }
     }
   }
+  
+  // TODO
+  static var preview: PersistenceController = {
+    let result = PersistenceController(inMemory: true)
+    let viewContext = result.container.viewContext
+    for index in 1..<6 {
+      let newItem = ExpenseModel(context: viewContext)
+      newItem.title = "Test Title \(index)"
+      newItem.date = Date(timeIntervalSinceNow: Double(index * -60))
+      newItem.comment = "Test Comment \(index)"
+      newItem.price = Double(index + 1) * 12.3
+      newItem.id = UUID()
+    }
+    do {
+      try viewContext.save()
+    } catch {
+      let nsError = error as NSError
+      fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+    }
+    return result
+  }()
+
+  static let previewItem: ExpenseModel = {
+    let newItem = ExpenseModel(context: preview.container.viewContext)
+    newItem.title = "Preview Item Title"
+    newItem.date = Date(timeIntervalSinceNow: 60)
+    newItem.comment = "Preview Item Comment"
+    newItem.price = 12.34
+    newItem.id = UUID()
+    return newItem
+  }()
+
+  
 }
