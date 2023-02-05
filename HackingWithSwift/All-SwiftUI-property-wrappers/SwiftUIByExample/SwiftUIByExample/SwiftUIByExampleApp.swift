5bfed4679c2b217516369a67449dce2f6e5b0cb8
MichaelChen <helloworldsmart3@gmail.com>
2023-02-05 18:23:47 +0800

    All SwiftUI property wrappers explained and compared

---
+++ HackingWithSwift/All-SwiftUI-property-wrappers/SwiftUIByExample/SwiftUIByExample/SwiftUIByExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIByExampleApp.swift
+//  SwiftUIByExample
+//
+//  Created by Michael on 2023/2/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIByExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
