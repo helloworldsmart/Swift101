5ee6305c34f303578629d19d1adb2af7384359e9
MichaelChen <helloworldsmart3@gmail.com>
2023-06-26 19:43:27 +0800

    Data Structures and Algorithms in Swift

---
+++ Data Structures and Algorithms in Swift/Chapter1.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var people = ["Brian", "Stanley", "Ringo"]
+
+people[0]
+people[1]
+people[2]
+
+people.append("Charles")
+print(people)
+
+people.insert("Andy", at: 0)
+
+var scores: [String: Int] = ["Eric": 9, "Mark": 12, "Wayne": 10]
+
+scores["Andrew"] = 0
