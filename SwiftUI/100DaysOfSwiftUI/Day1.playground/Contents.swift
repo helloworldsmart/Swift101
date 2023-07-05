665dfdcd8a02e87bef2e49240c2b1354970ed549
MichaelChen <helloworldsmart3@gmail.com>
2023-07-05 21:30:32 +0800

    How to create variables and constants ? / How to create strings ?

---
+++ SwiftUI/100DaysOfSwiftUI/Day1.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+//character = "Eloise"
+//character = "Francesca"
+
+var playerName = "Roy"
+print(playerName)
+playerName = "Dani"
+print(playerName)
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBread = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+let actor = "Denzel Washington"
+
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(movie.hasPrefix("a day"))
+print(filename.hasSuffix(".jpg"))
