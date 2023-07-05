2547742a23c170d682604df9210a6abf81ec0bb9
MichaelChen <helloworldsmart3@gmail.com>
2023-07-05 22:19:39 +0800

    How to store whole numbers

--- SwiftUI/100DaysOfSwiftUI/Day1.playground/Contents.swift
+++ SwiftUI/100DaysOfSwiftUI/Day1.playground/Contents.swift
@@ -57,3 +57,27 @@ Gang aft agley
 //var coleridge = """
 //Water, water, everywhere
 //and not a drop to drink"""
+
+let score = 10
+let reallyBig = 100_000_000
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubleScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+//counter = counter + 5
+counter += 5
+print(counter)
+counter *= 2
+print(counter)
+counter -= 10
+print(counter)
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
