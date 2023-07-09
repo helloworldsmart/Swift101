76b807b5701578091496676fd30498a867bea569
MichaelChen <helloworldsmart3@gmail.com>
2023-07-09 20:31:00 +0800

    How to store and find data in dictionaries ?

--- SwiftUI/100DaysOfSwiftUI/Day3.playground/Contents.swift
+++ SwiftUI/100DaysOfSwiftUI/Day3.playground/Contents.swift
@@ -52,3 +52,34 @@ let presidents = ["Bush", "Obama", "Trump", "Biden"]
 let reversedPresidents = presidents.reversed()
 print(reversedPresidents)
 
+let employee = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+
+print(employee["name", default: "Unknown"])
+print(employee["job", default: "Unknown"])
+print(employee["location", default: "Unknown"])
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false
+]
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo"
+]
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+archEnemies["Batman"] = "Penguin"
