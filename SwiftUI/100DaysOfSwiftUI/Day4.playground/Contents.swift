938cb917665946af315db4c3ed18a6f8cbdd3a36
MichaelChen <helloworldsmart3@gmail.com>
2023-07-14 18:57:58 +0800

    How to use type annotations ?

---
+++ SwiftUI/100DaysOfSwiftUI/Day4.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var surname: String = "Lasso"
+var score: Int = 0
+
+let playerName: String = "Roy"
+
+var luckyNumber: Int = 13
+
+let pi: Double = 3.141
+
+var isAuthenticated: Bool = true
+
+var albums: [String] = ["Red", "Fearless"]
+
+var user: [String: String] = ["id": "@twostraws"]
+
+var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
+
+var soda: [String] = ["Coke", "Pepsi", "Trn-Bru"]
+
+var teams: [String] = [String]()
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style = UIStyle.light
