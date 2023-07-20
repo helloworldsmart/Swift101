9c09f42767dc360d9c665d786613b91d67b90397
MichaelChen <helloworldsmart3@gmail.com>
2023-07-21 00:48:12 +0800

    How to check multiple conditions ?

--- SwiftUI/100DaysOfSwiftUI/Day5.playground/Contents.swift
+++ SwiftUI/100DaysOfSwiftUI/Day5.playground/Contents.swift
@@ -79,3 +79,32 @@ enum Sizes: Comparable {
 let first = Sizes.small
 let second = Sizes.large
 print(first < second)
+
+let temp = 25
+
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if userAge >= 18 || hasParentalConsent == true {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
