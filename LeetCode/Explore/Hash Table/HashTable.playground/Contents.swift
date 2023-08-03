c07ab33ef3c680cd2e9a5d5283ba65899207d6d8
MichaelChen <helloworldsmart3@gmail.com>
2023-08-03 23:03:57 +0800

    Design HashMap

--- LeetCode/Explore/Hash Table/HashTable.playground/Contents.swift
+++ LeetCode/Explore/Hash Table/HashTable.playground/Contents.swift
@@ -25,3 +25,41 @@ class MyHashSet {
         return set[hash(key)].contains(key)
     }
 }
+
+// Design HashMap
+
+class MyHashMap {
+    
+    private var buckets: [[(Int, Int)]]
+    private let bucketSize = 1000
+    
+    init() {
+        buckets = Array(repeating: [], count: bucketSize)
+    }
+    
+    func put(_ key: Int, _ value: Int) {
+        let hashValue = key % bucketSize
+        for (index, kv) in buckets[hashValue].enumerated() {
+            if kv.0 == key {
+                buckets[hashValue][index] = (key, value)
+                return
+            }
+        }
+        buckets[hashValue].append((key, value))
+    }
+    
+    func get(_ key: Int) -> Int {
+        let hashValue = key % bucketSize
+        for kv in buckets[hashValue] {
+            if kv.0 == key {
+                return kv.1
+            }
+        }
+        return -1
+    }
+    
+    func remove(_ key:  Int) {
+        let hashValue = key % bucketSize
+        buckets[hashValue].removeAll(where: {$0.0 == key})
+    }
+}
