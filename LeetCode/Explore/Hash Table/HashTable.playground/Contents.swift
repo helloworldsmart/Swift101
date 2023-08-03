89cd044e590fd803af7bc52a4e0d80890c9a7fd7
MichaelChen <helloworldsmart3@gmail.com>
2023-08-03 18:37:22 +0800

    Design HashSet

---
+++ LeetCode/Explore/Hash Table/HashTable.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// Design HashSet
+
+class MyHashSet {
+    init() {
+        
+    }
+    
+    func add(_ key: Int) {
+        
+    }
+    
+    func remove(_ key: Int) {
+        
+    }
+    
+    func contains(_ key: Int) -> Bool {
+        
+    }
+}
