5e68717fd0312ee614ffa2f824050961e4f98ecf
MichaelChen <helloworldsmart3@gmail.com>
2023-08-03 21:05:40 +0800

    Design HashSet

--- LeetCode/Explore/Hash Table/HashTable.playground/Contents.swift
+++ LeetCode/Explore/Hash Table/HashTable.playground/Contents.swift
@@ -3,19 +3,25 @@ import UIKit
 // Design HashSet
 
 class MyHashSet {
-    init() {
-        
+    private var set = [[Int]](repeating: [], count: 10000)
+    
+    private func hash(_ key: Int) -> Int {
+        return key % set.count
     }
     
     func add(_ key: Int) {
-        
+        if !contains(key) {
+            set[hash(key)].append(key)
+        }
     }
     
     func remove(_ key: Int) {
-        
+        if let index = set[hash(key)].firstIndex(of: key) {
+            set[hash(key)].remove(at: index)
+        }
     }
     
     func contains(_ key: Int) -> Bool {
-        
+        return set[hash(key)].contains(key)
     }
 }
