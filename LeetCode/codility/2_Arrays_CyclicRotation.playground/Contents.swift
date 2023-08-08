371ee6194c875629a8d7946e853ce1f514370e73
MichaelChen <helloworldsmart3@gmail.com>
2023-08-08 22:15:57 +0800

    codility_Arrays_CyclicRotation

---
+++ LeetCode/codility/2_Arrays_CyclicRotation.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+
+
+
+// -----
+// 上面程式在找尋二進制表示中的最大二進位間隙（binary gap），但在計算時可能遇到一些問題。程式在碰到每個 "1" 時增加 flag 變數，用來標記是否進入二進位間隙（當 flag 為 1 時），或是已經超過間隙（當 flag 為 2 時）。然而，這個計算方式可能有問題，尤其是在評估連續多個 "1" 的情況下。
+// -----
+
+public func solution2(_ N : Int) -> Int {
+    // Implement your solution here
+    var max = 0
+    var count = 0
+    let binaryString = String(N, radix: 2)
+    var flag = 0
+    var isClose = false
+    for char in binaryString {
+        if char == "1" {
+            flag += 1
+        }
+        if char == "0" && flag == 1 {
+            count += 1
+        }
+        if flag == 2 && count >= 1 && isClose == false {
+            if max < count {
+                max = count
+                isClose = true
+            }
+        }
+        if flag == 3 {
+            flag = 1
+            count = 0
+            isClose = false
+        }
+    }
+    return max
+}
+
+// -----
+// Task Score : 53%
+// Correctness: 53%
+// Performance: Not assessed
+// -----
