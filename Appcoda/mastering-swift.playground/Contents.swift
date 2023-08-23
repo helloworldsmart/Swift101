7474ad0bffa8d9279410537a955b96ca4ba75c73
MichaelChen <helloworldsmart3@gmail.com>
2023-08-23 22:21:52 +0800

    Generics

--- Appcoda/mastering-swift.playground/Contents.swift
+++ Appcoda/mastering-swift.playground/Contents.swift
@@ -1,3 +1,4 @@
+import Foundation
 import UIKit
 
 // Enumerations
@@ -87,4 +88,93 @@ let doubledNumbersOver8 = chainNumbers.compactMap { $0 }.filter { $0 > 3 }.map {
 print(doubledNumbersOver8)
 
 // Generics
+func swapInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryB = b
+    b = a
+    a = temporaryB
+}
 
+var num1 = 10
+var num2 = 20
+
+swapInts(&num1, &num2)
+print(num1)
+print(num2)
+
+func swapStrings(_ a: inout String, _ b: inout String) {
+    let temporaryB = b
+    b = a
+    a = temporaryB
+}
+
+func swapAnything<T>(_ a: inout T, _ b: inout T) {
+    let temporaryB = b
+    b = a
+    a = temporaryB
+}
+
+var string1 = "Happy"
+var string2 = "New Year"
+
+swapAnything(&string1, &string2)
+print(string1)
+print(string2)
+
+var bool1 = false
+var bool2 = true
+
+swapAnything(&bool1, &bool2)
+print(bool1)
+print(bool2)
+
+struct Stack {
+    private var storage = [Any]()
+    
+    mutating func push(_ item: Any) {
+        storage.append(item)
+    }
+    
+    mutating func pop() -> Any? {
+        return storage.removeLast()
+    }
+}
+
+var myStack = Stack()
+myStack.push("foo")
+myStack.push(5)
+myStack.push(4.7)
+
+let element = myStack.pop()
+
+switch element {
+case let number as Int: print(number)
+case let number as Double: print(number)
+case let text as String: print(text)
+default: print("Unknown type")
+}
+
+struct GenericStack<Element> {
+    private var storage = [Element]()
+    
+    mutating func push(_ item: Element) {
+        storage.append(item)
+    }
+    
+    mutating func pop() -> Element? {
+        return storage.removeLast()
+    }
+}
+
+var textStack = GenericStack<String>()
+textStack.push("foo")
+textStack.push("bar")
+textStack.push("baz")
+
+let textElement = textStack.pop()
+
+var numStack = GenericStack<Int>()
+numStack.push(10)
+numStack.push(20)
+numStack.push(30)
+
+let numElement = numStack.pop()
