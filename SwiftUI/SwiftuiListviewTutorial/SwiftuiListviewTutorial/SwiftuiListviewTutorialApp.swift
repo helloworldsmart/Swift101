0413db4ffd3998f7a3adbd77cb83cc2e22f004d1
MichaelChen <helloworldsmart3@gmail.com>
2023-09-06 22:51:27 +0800

    Add: SwiftUI Listview Tutorial

---
+++ SwiftUI/SwiftuiListviewTutorial/SwiftuiListviewTutorial/SwiftuiListviewTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiListviewTutorialApp.swift
+//  SwiftuiListviewTutorial
+//
+//  Created by Michael on 2023/9/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiListviewTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
