31a7f791456502c639affd67ff6cfd5931e24721
michaelchenfun <michael.gf.chen@tripresso.com>
2023-09-16 22:24:54 +0800

    Feat: Swimming Strokes App

---
+++ 2023ithome/SwimmingStrokes/SwimmingStrokes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwimmingStrokes
+//
+//  Created by Michael on 2023/9/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2023ithome/SwimmingStrokes/SwimmingStrokes/SwimmingStrokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimmingStrokesApp.swift
+//  SwimmingStrokes
+//
+//  Created by Michael on 2023/9/16.
+//
+
+import SwiftUI
+
+@main
+struct SwimmingStrokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
