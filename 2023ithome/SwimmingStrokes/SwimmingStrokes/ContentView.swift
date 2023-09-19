e273c8e09ab6aed15b80b13649def4aac6c9eeb2
MichaelChen <helloworldsmart3@gmail.com>
2023-09-19 20:37:38 +0800

    feat: SwimmingStrokes

--- 2023ithome/SwimmingStrokes/SwimmingStrokes/ContentView.swift
+++ 2023ithome/SwimmingStrokes/SwimmingStrokes/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["ArtisticSwimming", "Backstroke", "Breaststroke", "Butterfly", "Freestyle"]
+
+    @State private var selected = "ArtisticSwimming"
+    @State private var id = 1
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Why not try…")
+                    .font(.largeTitle.bold())
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image("\(selected)")
+                            .resizable()
+                            .clipShape(Circle())
+    //                        .font(.system(size: 144))
+    //                        .foregroundColor(.white)
+                    )
+    //
+    //            Text("ArtisticSwimming!")
+    //                .font(.title)
+                Text("\(selected)!")
+                    .font(.title)
+            }
+            Button("Try again") {
+                // change activity
+                withAnimation(.easeInOut(duration: 1)) {
+                    selected = activities.randomElement() ?? "ArtisticSwimming"
+                    id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
     }
 }
 
