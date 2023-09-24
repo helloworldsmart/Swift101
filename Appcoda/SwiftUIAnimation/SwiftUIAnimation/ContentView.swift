bbce282d9d34a22e624aaae107d12ab4fd066b2c
MichaelChen <helloworldsmart3@gmail.com>
2023-09-24 15:08:24 +0800

    feat: circle download Indicator

--- Appcoda/SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
+++ Appcoda/SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -8,36 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var circleColorChanged = false
-    @State private var heartColorChanged = false
-    @State private var heartSizeChanged = false
-    
+
+    @State private var isLoading = false
     
     var body: some View {
-        
         ZStack {
             Circle()
-                .frame(width: 200, height: 200)
-                .foregroundColor(circleColorChanged ? Color(.systemGray5) : .red)
-//                .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
+                .stroke(Color(.systemGray5), lineWidth: 14)
+                .frame(width: 100, height: 100)
             
-            Image(systemName: "heart.fill")
-                .foregroundColor(heartColorChanged ? .red : .white)
-                .font(.system(size: 100))
-                .animation(nil)
-                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
-//                .animation(.default)
-        }
-//        .animation(.default)
-        .onTapGesture {
-            withAnimation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)) {
-                self.circleColorChanged.toggle()
-                self.heartColorChanged.toggle()
-            }
-            self.heartSizeChanged.toggle()
+            Circle()
+                .trim(from: 0, to: 0.2)
+                .stroke(Color.green, lineWidth: 7)
+                .frame(width: 100, height: 100)
+                .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+    //            .animation(Animation.default.repeatForever(autoreverses: false))
+                .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                .onAppear() {
+                    self.isLoading = true
+                }
         }
     }
 }
+
+//struct ContentView: View {
+//    @State private var circleColorChanged = false
+//    @State private var heartColorChanged = false
+//    @State private var heartSizeChanged = false
+//
+//
+//    var body: some View {
+//
+//        ZStack {
+//            Circle()
+//                .frame(width: 200, height: 200)
+//                .foregroundColor(circleColorChanged ? Color(.systemGray5) : .red)
+////                .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
+//
+//            Image(systemName: "heart.fill")
+//                .foregroundColor(heartColorChanged ? .red : .white)
+//                .font(.system(size: 100))
+//                .animation(nil)
+//                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
+////                .animation(.default)
+//        }
+////        .animation(.default)
+//        .onTapGesture {
+//            withAnimation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)) {
+//                self.circleColorChanged.toggle()
+//                self.heartColorChanged.toggle()
+//            }
+//            self.heartSizeChanged.toggle()
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
