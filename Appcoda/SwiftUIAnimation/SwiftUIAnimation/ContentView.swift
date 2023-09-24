e3517d5b49584997412f4b4b05e016002817f2f7
MichaelChen <helloworldsmart3@gmail.com>
2023-09-24 15:13:47 +0800

    fix: CircleDownloadIndicatorView, HeartView

--- Appcoda/SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
+++ Appcoda/SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var body: some View {
+        CircleDownloadIndicatorView()
+        HeartView()
+    }
+}
+
+struct CircleDownloadIndicatorView: View {
 
     @State private var isLoading = false
     
@@ -31,37 +39,37 @@ struct ContentView: View {
     }
 }
 
-//struct ContentView: View {
-//    @State private var circleColorChanged = false
-//    @State private var heartColorChanged = false
-//    @State private var heartSizeChanged = false
-//
-//
-//    var body: some View {
-//
-//        ZStack {
-//            Circle()
-//                .frame(width: 200, height: 200)
-//                .foregroundColor(circleColorChanged ? Color(.systemGray5) : .red)
-////                .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
-//
-//            Image(systemName: "heart.fill")
-//                .foregroundColor(heartColorChanged ? .red : .white)
-//                .font(.system(size: 100))
-//                .animation(nil)
-//                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
-////                .animation(.default)
-//        }
-////        .animation(.default)
-//        .onTapGesture {
-//            withAnimation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)) {
-//                self.circleColorChanged.toggle()
-//                self.heartColorChanged.toggle()
-//            }
-//            self.heartSizeChanged.toggle()
-//        }
-//    }
-//}
+struct HeartView: View {
+    @State private var circleColorChanged = false
+    @State private var heartColorChanged = false
+    @State private var heartSizeChanged = false
+
+
+    var body: some View {
+
+        ZStack {
+            Circle()
+                .frame(width: 200, height: 200)
+                .foregroundColor(circleColorChanged ? Color(.systemGray5) : .red)
+//                .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
+
+            Image(systemName: "heart.fill")
+                .foregroundColor(heartColorChanged ? .red : .white)
+                .font(.system(size: 100))
+                .animation(nil)
+                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
+//                .animation(.default)
+        }
+//        .animation(.default)
+        .onTapGesture {
+            withAnimation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)) {
+                self.circleColorChanged.toggle()
+                self.heartColorChanged.toggle()
+            }
+            self.heartSizeChanged.toggle()
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
