7763d2b5b0182255b9168c99dcd33e317d27791d
MichaelChen <helloworldsmart3@gmail.com>
2023-09-24 15:25:24 +0800

    feat: Rectangle Download Indicator View

--- Appcoda/SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
+++ Appcoda/SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -10,8 +10,41 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
+        Spacer()
         CircleDownloadIndicatorView()
+        Spacer()
         HeartView()
+        Spacer()
+        RectangleDownloadIndicatorView()
+        Spacer()
+    }
+}
+
+struct RectangleDownloadIndicatorView: View {
+    
+    @State private var isLoading = false
+    
+    var body: some View {
+        ZStack {
+            
+            Text("Loading")
+                .font(.system(.body, design: .rounded))
+                .bold()
+                .offset(x: 0, y: -25)
+            
+            RoundedRectangle(cornerRadius: 3)
+                .stroke(Color(.systemGray5), lineWidth: 3)
+                .frame(width: 250, height: 3)
+            
+            RoundedRectangle(cornerRadius: 3)
+                .stroke(Color.green, lineWidth: 3)
+                .frame(width: 30, height: 3)
+                .offset(x: isLoading ? 110 : -110, y : 0)
+                .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+        }
+        .onAppear() {
+            self.isLoading = true
+        }
     }
 }
 
