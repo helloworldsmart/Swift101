12deedf488afe6b49d8153a563f237724df009a0
MichaelChen <helloworldsmart3@gmail.com>
2023-09-30 17:00:37 +0800

    feat: SwiftUI TabView tutorial

--- Appcoda/FriendsTravel/FriendsTravel/ContentView.swift
+++ Appcoda/FriendsTravel/FriendsTravel/ContentView.swift
@@ -39,9 +39,23 @@ struct ContentView: View {
                 .tabItem {
                     Label("Notification", systemImage: "bell")
                 }
+
+            // The following tabs will be grouped under the More tab.
             Text("Settings")
                 .tabItem {
                     Label("Settings", systemImage: "gershape")
+                }
+            Text("Extra 1")
+                .tabItem {
+                    Label("Extra 1", systemImage: "1.circle")
+                }
+            Text("Extra 2")
+                .tabItem {
+                    Label("Extra 2", systemImage: "2.circle")
+                }
+            Text("Extra 3")
+                .tabItem {
+                    Label("Extra 3", systemImage: "3.circle")
                 }
         }
     }
