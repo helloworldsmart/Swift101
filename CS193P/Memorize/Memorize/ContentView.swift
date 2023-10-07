902f7161fd469ceb6eca950bdee17af28e5e91ee
MichaelChen <helloworldsmart3@gmail.com>
2023-10-07 21:44:45 +0800

    feat: CS193P Lecture 1

--- CS193P/Memorize/Memorize/ContentView.swift
+++ CS193P/Memorize/Memorize/ContentView.swift
@@ -8,40 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    var viewModel: EmojiMemoryGame
+    
     var body: some View {
-      HStack {
-        ForEach(viewModel.cards) { card in
-//          CardView(isFaceUp: false)
-          CardView(card: card).onTapGesture {
-            self.viewModel.choose(card: card)
-          }
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
-      }
+        .foregroundColor(.orange)
         .padding()
-        .foregroundColor(Color.orange)
-        .font(Font.largeTitle)
     }
 }
 
 struct CardView: View {
-//  var isFaceUp: Bool
-  var card: MemoryGame<String>.Card
-  var body: some View {
-    ZStack {
-      if card.isFaceUp {
-        RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-        RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-        Text(card.content)
-      } else {
-        RoundedRectangle(cornerRadius: 10.0).fill()
-      }
-   }
-  }
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("🥳").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(viewModel: EmojiMemoryGame())
+        ContentView()
     }
 }
