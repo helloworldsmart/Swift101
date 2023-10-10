c05992dee637a9c30e56c9faff2dd14bc35bd472
MichaelChen <helloworldsmart3@gmail.com>
2023-10-10 18:06:11 +0800

    feat: UIViewRepresentable

--- 2023ithome/SwimmingStrokes/SwimmingStrokes/Settings/TextView.swift
+++ 2023ithome/SwimmingStrokes/SwimmingStrokes/Settings/TextView.swift
@@ -29,19 +29,30 @@ import SwiftUI
 //
 struct TextView: UIViewRepresentable {
     
-    func makeUIView(context: Context) -> UITableView {
-        // Return the UIView object
-        return UITableView()
+    @Binding var text: String
+    @Binding var textStyle: UIFont.TextStyle
+    
+    func makeUIView(context: Context) -> UITextView {
+        let textView = UITextView()
+        
+        textView.font = UIFont.preferredFont(forTextStyle: textStyle)
+        textView.autocapitalizationType = .sentences
+        textView.isSelectable = true
+        textView.isUserInteractionEnabled = true
+
+        return textView
     }
     
-    func updateUIView(_ uiView: UITableView, context: Context) {
+    func updateUIView(_ uiView: UITextView, context: Context) {
         // Update the view
+        uiView.text = text
+        uiView.font = UIFont.preferredFont(forTextStyle: textStyle)
     }
     
 }
 
-struct TextView_Previews: PreviewProvider {
-    static var previews: some View {
-        TextView()
-    }
-}
+//struct TextView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TextView(text: "", textStyle: UIFont.TextStyle.body)
+//    }
+//}
