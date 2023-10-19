a5491a04d082d01309e0a7b5ab73fca9ca016fa2
MichaelChen <helloworldsmart3@gmail.com>
2023-10-19 22:28:40 +0800

    feat: Query Enum

---
+++ DesignCode/VideoPlayerDemo/VideoPlayerDemo/VideoManager.swift
@@ -0,0 +1,12 @@
+//
+//  VideoManager.swift
+//  VideoPlayerDemo
+//
+//  Created by Michael on 2023/10/19.
+//
+
+import Foundation
+
+enum Query: String, CaseIterable {
+    case nature, animals, people, ocean, food
+}
---
+++ DesignCode/VideoPlayerDemo/VideoPlayerDemo/Views/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  VideoPlayerDemo
+//
+//  Created by Michael on 2023/10/17.
+//
+
+import SwiftUI
+import AVKit
+
+//struct ContentView: View {
+//    @State var player = AVPlayer()
+//    var videoUrl: String = "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4"
+//    var body: some View {
+//        VideoPlayer(player: player)
+//            .onAppear() {
+//                player = AVPlayer(url: URL(string: videoUrl)!)
+//            }
+//    }
+//}
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(Query.allCases, id: \.self) { searchQuery in
+                    QueryTag(query: searchQuery, isSelected: false)
+                }
+            }
+        }
+        .background(Color("AccentColor"))
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
