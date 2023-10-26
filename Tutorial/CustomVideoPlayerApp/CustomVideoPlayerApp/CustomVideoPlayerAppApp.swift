9fa903fa1e31d4f8414d5325309d96af2a93dbfe
MichaelChen <helloworldsmart3@gmail.com>
2023-10-26 22:05:48 +0800

    CustomVideoPlayer init

---
+++ Tutorial/CustomVideoPlayerApp/CustomVideoPlayerApp/CustomVideoPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomVideoPlayerAppApp.swift
+//  CustomVideoPlayerApp
+//
+//  Created by Michael on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomVideoPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
