cc94d08f499e33aa3870ba7d5b81b230a6790fbb
MichaelChen <helloworldsmart3@gmail.com>
2023-10-26 22:42:52 +0800

    feat: Project Setup

---
+++ Tutorial/CustomVideoPlayerApp/CustomVideoPlayerApp/View/Home.swift
@@ -0,0 +1,24 @@
+//
+//  Home.swift
+//  CustomVideoPlayerApp
+//
+//  Created by Michael on 2023/10/26.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var size: CGSize
+    var safeArea: EdgeInsets
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
