ffb5eced3ab9bd6037cee215a9d3866c4ac64f5a
MichaelChen <helloworldsmart3@gmail.com>
2023-10-31 20:20:14 +0800

    feat: Calling OnTap Function After Animation has been initiated

--- Tutorial/CustomVideoPlayerApp/CustomVideoPlayerApp/View/DoubleTapSeek.swift
+++ Tutorial/CustomVideoPlayerApp/CustomVideoPlayerApp/View/DoubleTapSeek.swift
@@ -12,6 +12,10 @@ struct DoubleTapSeek: View {
     var isForward: Bool = false
 //    var onTap: Bool = false
     var onTap: () -> ()
+    /// Animation Properties
+    @State private var isTapped: Bool = false
+    /// Since we have three arrows
+    @State private var showArrows: [Bool] = [false, false, false]
     var body: some View {
         Rectangle()
             .foregroundColor(.clear)
@@ -20,17 +24,50 @@ struct DoubleTapSeek: View {
                     .fill(.black.opacity(0.4))
                     .scaleEffect(2, anchor: isForward ? .leading : .trailing)
             }
+            .opacity(isTapped ? 1 : 0)
             /// Arrows
             .overlay {
                 VStack(spacing: 10) {
                     HStack(spacing: 0) {
-                        ForEach(0..<2, id: \.self) { index in
+                        ForEach(0...2, id: \.self) { index in
                             Image(systemName: "arrowtriangle.backward.fill")
+                                .opacity(showArrows[index] ? 1 : 0.2)
                         }
                     }
                     .font(.title)
                     .rotationEffect(.init(degrees: isForward ? 180 : 0))
+                    
+                    Text("15 Seconds")
+                        .font(.caption)
+                        .fontWeight(.semibold)
                 }
+                /// Showing only if tapped
+                .opacity(isTapped ? 1 : 0)
+            }
+            .contentShape(Rectangle())
+            .onTapGesture {
+                withAnimation(.easeInOut(duration: 0.25)) {
+                    isTapped = true
+                    showArrows[0] = true
+                }
+                
+                withAnimation(.easeInOut(duration: 0.2).delay(0.2)) {
+                    showArrows[0] = false
+                    showArrows[1] = true
+                }
+                
+                withAnimation(.easeInOut(duration: 0.2).delay(0.35)) {
+                    showArrows[1] = false
+                    showArrows[2] = true
+                }
+                
+                withAnimation(.easeInOut(duration: 0.2).delay(0.5)) {
+                    showArrows[2] = false
+                    isTapped = false
+                }
+                
+                /// Calling OnTap Function After Animation has been initiated
+                onTap()
             }
     }
 }
